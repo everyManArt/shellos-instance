f07f45071dd45cc083c013c110ee8d2dca36e583
Jonny Andrews <jonny.andrews@GL04325M.local>
2014-06-25 19:39:56 +0200

    asset change

--- onMinute.sh
+++ onMinute.sh
@@ -5,8 +5,7 @@
 doActions(){
 	## Put actions here
 
-	##playMedia "http://i.imgur.com/dAtcCfH.gif"
-	
+	playMedia "http://i.imgur.com/dAtcCfH.gif"
 	playMedia "http://i.imgur.com/VmGaKOG.gif";
 	playMedia "http://i.imgur.com/NOJMaGg.gif";
 	playMedia "http://i.imgur.com/nBTu3oi.gif";
