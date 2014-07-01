b39c331df766d08a3f44de6857836deec7e93af2
jonnyAnd <jonny@eino.co.uk>
2014-07-01 17:22:50 +0200

    some nice new media

--- onMinute.sh
+++ onMinute.sh
@@ -12,6 +12,21 @@ doActions(){
 	playMedia "http://i.imgur.com/nBTu3oi.gif" true;
 	playMedia "http://i.imgur.com/vzokKQ9.gif" true;
 	
+	
+	playMedia "http://giant.gfycat.com/PiercingWelcomeElephantbeetle.gif" true
+	playMedia "http://i.imgur.com/XSy47dI.gif" true;
+	playMedia "http://i.imgur.com/ZUhjayk.gif" true;
+	playMedia "http://i.imgur.com/JfkmXjG.gif" true;
+	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr06/1/6/anigif_enhanced-buzz-26657-1380623142-0.gif" true;
+	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr06/1/6/anigif_enhanced-buzz-10545-1380624355-4.gif" true;
+	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr02/1/7/anigif_enhanced-buzz-8611-1380625430-4.gif" true;
+	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr06/1/11/anigif_enhanced-buzz-30767-1380641248-13.gif" true;
+	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr02/1/11/anigif_enhanced-buzz-15300-1380642491-15.gif" true;
+	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr05/1/11/anigif_original-grid-image-20611-1380642921-25.gif" true;
+	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr01/1/12/anigif_enhanced-buzz-13824-1380644864-1.gif" true;
+	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr06/1/12/anigif_enhanced-buzz-8788-1380646258-32.gif" true;
+	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr02/2/5/anigif_enhanced-buzz-12672-1380705549-14.gif" true;
+	
 	##playMedia "http://i.imgur.com/2CCIIMk.gif";
 	
 	
