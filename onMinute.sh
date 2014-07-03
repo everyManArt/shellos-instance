31ddd0ba4fad1e5daf93dfd6773230fdf5bac8b5
Jonny Andrews <jonny.andrews@GL04325M.local>
2014-07-03 15:22:37 +0200

    adding test you tube vid

--- onMinute.sh
+++ onMinute.sh
@@ -4,28 +4,30 @@
 
 doActions(){
 	## Put actions here
+	playYouTube "1FH-q0I1fJY"
+
 
 	##playMedia "http://radio.7chan.org:8000/CH7"
 	
-	playMedia "http://i.imgur.com/VmGaKOG.gif" true;
-	playMedia "http://i.imgur.com/NOJMaGg.gif" true;
-	playMedia "http://i.imgur.com/nBTu3oi.gif" true;
-	playMedia "http://i.imgur.com/vzokKQ9.gif" true;
+##	playGif "http://i.imgur.com/VmGaKOG.gif" ;
+##	playGif "http://i.imgur.com/NOJMaGg.gif" ;
+##	playGif "http://i.imgur.com/nBTu3oi.gif" ;
+##	playGif "http://i.imgur.com/vzokKQ9.gif" ;
 	
 	
-	playMedia "http://giant.gfycat.com/PiercingWelcomeElephantbeetle.gif" true
-	playMedia "http://i.imgur.com/XSy47dI.gif" true;
-	playMedia "http://i.imgur.com/ZUhjayk.gif" true;
-	playMedia "http://i.imgur.com/JfkmXjG.gif" true;
-	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr06/1/6/anigif_enhanced-buzz-26657-1380623142-0.gif" true;
-	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr06/1/6/anigif_enhanced-buzz-10545-1380624355-4.gif" true;
-	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr02/1/7/anigif_enhanced-buzz-8611-1380625430-4.gif" true;
-	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr06/1/11/anigif_enhanced-buzz-30767-1380641248-13.gif" true;
-	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr02/1/11/anigif_enhanced-buzz-15300-1380642491-15.gif" true;
-	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr05/1/11/anigif_original-grid-image-20611-1380642921-25.gif" true;
-	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr01/1/12/anigif_enhanced-buzz-13824-1380644864-1.gif" true;
-	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr06/1/12/anigif_enhanced-buzz-8788-1380646258-32.gif" true;
-	playMedia "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr02/2/5/anigif_enhanced-buzz-12672-1380705549-14.gif" true;
+##	playGif "http://giant.gfycat.com/PiercingWelcomeElephantbeetle.gif" 
+##	playGif "http://i.imgur.com/XSy47dI.gif" ;
+##	playGif "http://i.imgur.com/ZUhjayk.gif" ;
+##	playGif "http://i.imgur.com/JfkmXjG.gif" ;
+##	playGif "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr06/1/6/anigif_enhanced-buzz-26657-1380623142-0.gif" ;
+##	playGif "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr06/1/6/anigif_enhanced-buzz-10545-1380624355-4.gif" ;
+##	playGif "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr02/1/7/anigif_enhanced-buzz-8611-1380625430-4.gif" ;
+##	playGif "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr06/1/11/anigif_enhanced-buzz-30767-1380641248-13.gif" ;
+##	playGif "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr02/1/11/anigif_enhanced-buzz-15300-1380642491-15.gif" ;
+##	playGif "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr05/1/11/anigif_original-grid-image-20611-1380642921-25.gif" ;
+##	playGif "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr01/1/12/anigif_enhanced-buzz-13824-1380644864-1.gif" ;
+##	playGif "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr06/1/12/anigif_enhanced-buzz-8788-1380646258-32.gif" ;
+##	playGif "http://s3-ec.buzzfed.com/static/2013-10/enhanced/webdr02/2/5/anigif_enhanced-buzz-12672-1380705549-14.gif" ;
 	
 	##playMedia "http://i.imgur.com/2CCIIMk.gif";
 	
@@ -34,10 +36,6 @@ doActions(){
 	
 	
 
-	##playMedia "http://media2.giphy.com/media/XNzYBOhw7C3mw/giphy.gif"
-	##wait 5
-	##playMedia "http://media.giphy.com/media/x5aebCMe2PUCA/giphy.gif"
-	
 
 	## when done go home
 	allActionsComplete
