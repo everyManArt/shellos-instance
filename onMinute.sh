d89c876e13687da7b7e3e3bedb0987e28c082429
jonnyAnd <jonny@eino.co.uk>
2014-07-30 15:01:07 +0200

    back to stream

--- onMinute.sh
+++ onMinute.sh
@@ -9,12 +9,12 @@ doActions(){
 
 ## Example youtube videos
 	##playYouTube "1FH-q0I1fJY" ##BROKEN ?
-	playYouTube "E5fccx15b2k"
-	playYouTube "J7E-aoXLZGY"
-	playYouTube "wQGn4wTIFs4"
+	##playYouTube "E5fccx15b2k"
+	##playYouTube "J7E-aoXLZGY"
+	##playYouTube "wQGn4wTIFs4"
 
 ##This playes 4Chan TV, who knew?
-	##playMedia "http://radio.7chan.org:8000/CH7"
+	playMedia "http://radio.7chan.org:8000/CH7"
 
 
 
