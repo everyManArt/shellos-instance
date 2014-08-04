503132d385b1d8f9ea8f190b5c53b912fc7f6dab
jonnyAnd <jonny@eino.co.uk>
2014-08-04 19:17:52 +0200

    adding youtube preload

--- onLoad.sh
+++ onLoad.sh
@@ -3,6 +3,12 @@
 
 doActions(){
 	## Put actions here
+	
+	##populate youtube list
+	playYouTube "7_3QteCI7Pk"
+	youtubeUtil_youtubeVideoSearchResultsToFile "war" 
+	
+	
 
 	## when done go home
 	allActionsComplete
