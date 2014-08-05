645e78a5f5468348c5d4887b6150b7905858e7c1
jonnyAnd <jonny@eino.co.uk>
2014-08-05 10:26:04 +0200

    Update onHour.sh

--- onHour.sh
+++ onHour.sh
@@ -3,6 +3,10 @@
 
 doActions(){
 	## Put actions here
+	
+	playYouTube "8o0dYPfN-PM"
+	echo "Pre-loading youtube search"
+	youtubeUtil_youtubeVideoSearchResultsToFile $(dictionaryUtil_getRandomWordFromFile "./core/resources/dictionary.txt") 
 
 	## when done go home
 	allActionsComplete
