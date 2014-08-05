51f3ac79a9a56ad77f5b0722ff638237684afc6b
jonnyAnd <jonny@eino.co.uk>
2014-08-05 10:45:01 +0200

    Update onLoad.sh

--- onLoad.sh
+++ onLoad.sh
@@ -6,9 +6,13 @@ doActions(){
 	
 	playYouTube "8o0dYPfN-PM"
 	echo "Pre-loading youtube search"
-##	youtubeUtil_youtubeVideoSearchResultsToFile youtubeUtil_youtubeVideoSearchResultsToFile $(dictionaryUtil_getRandomWordFromFile "./core/resources/dictionary.txt"); 
-	youtubeUtil_youtubeVideoSearchResultsToFile $(dictionaryUtil_getRandomWordFromFile "./core/resources/dictionary.txt"); 	
 	
+	RAND_WORD=$(dictionaryUtil_getRandomWordFromFile "./core/resources/dictionary.txt")
+	
+	echo "Searching with word:"$RAND_WORD
+	
+	
+	youtubeUtil_youtubeVideoSearchResultsToFile $RAND_WORD 
 
 	## when done go home
 	allActionsComplete
