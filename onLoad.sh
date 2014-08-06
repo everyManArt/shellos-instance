5c00659e571dff30fd4490accdfd175ba8debb5b
jonnyAnd <jonny@eino.co.uk>
2014-08-06 18:03:58 +0200

    Update onLoad.sh

--- onLoad.sh
+++ onLoad.sh
@@ -15,7 +15,7 @@ doActions(){
 	
 	
 	##youtubeUtil_youtubeVideoSearchResultsToFile $RAND_WORD 
-	youtubeUtil_youtubeVideoSearchResultsToFile "amazing explosions" 
+	youtubeUtil_youtubeVideoSearchResultsToFile "amazing+explosions" 
 
 	## when done go home
 	allActionsComplete
