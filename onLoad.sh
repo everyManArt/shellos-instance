5f1c0bf6c75e3e932fcb3ef9706620e089eb3c9d
jonnyAnd <jonny@eino.co.uk>
2014-08-12 10:50:30 +0200

    Update onLoad.sh

--- onLoad.sh
+++ onLoad.sh
@@ -6,7 +6,7 @@ doActions(){
 	
 	clearPrecacheFile
 	
-	playGif "http://geekologie.com/2014/08/07/japanese-face-exerciser.gif";
+	playYouTube "8o0dYPfN-PM"
 	echo "Pre-loading youtube search"
 	
 	RAND_WORD=$(dictionaryUtil_getRandomWordFromFile "./core/resources/dictionary.txt")
