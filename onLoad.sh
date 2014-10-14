1fb1b867b5e8a5b1cd063a11c50aaadb52942107
everyManArt <every.Oliver@googlemail.com>
2014-10-14 23:45:20 +0100

    clean up for ollie

--- onLoad.sh
+++ onLoad.sh
@@ -2,20 +2,21 @@
 
 
 doActions(){
-	## Put actions here
-	
+
+	## this line will remove locally saved stuff, good to save space, bad for downloads
 	clearPrecacheFile
 	
-	playYouTube "8o0dYPfN-PM"
+	playYouTube "ir50v0UZEIE"
 	echo "Pre-loading youtube search"
 	
-	RAND_WORD=$(dictionaryUtil_getRandomWordFromFile "./core/resources/dictionary.txt")
+
 	
-	echo "Searching with word:"$RAND_WORD
+	## these three lines will get a random word from a dictionary and search youtube for them, save to list for later
+	RAND_WORD=$(dictionaryUtil_getRandomWordFromFile "./core/resources/dictionary.txt");
+	echo "Searching with word:"$RAND_WORD;
+	youtubeUtil_youtubeVideoSearchResultsToFile $RAND_WORD
 	
-	
-	youtubeUtil_youtubeVideoSearchResultsToFile $RAND_WORD 
-	##youtubeUtil_youtubeVideoSearchResultsToFile "abscond" 
+
 
 	## when done go home
 	allActionsComplete
