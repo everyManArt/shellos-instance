2b4fe76dc9395344f76a1b4bac31c332cb35c533
everyManArt <every.Oliver@googlemail.com>
2014-10-14 23:53:34 +0100

    Update onLoad.sh

--- onLoad.sh
+++ onLoad.sh
@@ -11,10 +11,10 @@ doActions(){
 	
 
 	
-	## these three lines will get a random word from a dictionary and search youtube for them, save to list for later
-	RAND_WORD=$(dictionaryUtil_getRandomWordFromFile "./core/resources/dictionary.txt");
-	echo "Searching with word:"$RAND_WORD;
-	youtubeUtil_youtubeVideoSearchResultsToFile $RAND_WORD
+## these three lines will get a random word from a dictionary and search youtube for them, save to list for later
+	## RAND_WORD=$(dictionaryUtil_getRandomWordFromFile "./core/resources/dictionary.txt");
+	## echo "Searching with word:"$RAND_WORD;
+	## youtubeUtil_youtubeVideoSearchResultsToFile $RAND_WORD
 	
 
 
